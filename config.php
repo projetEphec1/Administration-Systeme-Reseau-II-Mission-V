f4baa9ffcfb8752acf18e43f8fa0c100ce9a3d9b
projetEphec1 <nadiam005@yahoo.fr>
2017-06-04 20:01:46 +0200

    Create  config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocj1ht5wzrmu',
+  'passwordsalt' => 't8Ox3rVt+psCZx5h1x2m+0Aol1XRvz',
+  'secret' => 'oQHUxO9nwWEsc85BSfi/PUkIpR3tfs9fTArRHN6bEXnOP8Jl',
+  'trusted_domains' =>
+  array (
+    0 => '151.80.119.137',
+    1 => '79.137.39.47',
+    2 => 'owncloud.wt8.ephec-ti.be',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://151.80.119.137',
+  'dbtype' => 'sqlite3',
+  'version' => '9.1.6.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
